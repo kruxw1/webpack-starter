cdabb5b639b31072bb65a9ebb2826db7f45d3f63
Kevin Rux <kevrux@gmail.com>
2018-05-22 15:13:56 -0400

    - adding css minification - adding html template config

--- webpack.common.js
+++ webpack.common.js
@@ -1,21 +1,31 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Webpack starter kit'
-    })
+      title: 'Webpack starter kit',
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      // filename: '[name].css',
+      // chunkFilename: '[id].css'
+    }),
+    new OptimizeCSSAssetsPlugin({})
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           {
             loader: 'css-loader',
             options: {
@@ -53,7 +63,7 @@ module.exports = {
     ]
   },
   resolve: {
-    modules: [path.resolve(__dirname, "src"), "node_modules"],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
     extensions: ['.css', '.js', '.json']
   }
 };
